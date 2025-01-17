4bcdb8954f72ce22425e6a6ef4f7bed412987a13
ilexx <stealthfear@gmail.com>
2025-01-17 17:36:59 +0500

    Подготовка элементов формы LevelDataPageView

--- PryamolineynostNew/ViewModels/Level/LevelDataPageViewModel.cs
+++ PryamolineynostNew/ViewModels/Level/LevelDataPageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using PryamolineynostNew.Models.LevelTool;
 
 namespace PryamolineynostNew.ViewModels
@@ -11,10 +12,25 @@ namespace PryamolineynostNew.ViewModels
         public ICommand AddRowCommand { get; }
         public ICommand DeleteRowCommand { get; }
         
+        
+  
+        public int SelectedToolIndex { get; set; }
+
         public ObservableCollection<LevelDataItem> LevelData { get; set; }
 
         [ObservableProperty]
         private List<DataRow> _dataRows;
+
+        [ObservableProperty]
+        private string _selectedTool;
+
+
+        [ObservableProperty]
+        private static ObservableCollection<string> _toolsNameList = new ObservableCollection<string>
+        {
+            "Уровень",
+            "Автоколлиматор"
+        };
 
 
         public LevelDataPageViewModel(List<Models.LevelTool.DataRow> dataRows)
@@ -25,9 +41,22 @@ namespace PryamolineynostNew.ViewModels
 
         public class LevelDataItem
         {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public double Value { get; set; }
+            public int PointNumber { get; }
+            public int PointPosition { get; }
+            public double ActualProfilePoint { get; }
+            public decimal AdjStraightPoint { get; }
+            public decimal DeviationSize { get; }
+            public decimal DeviationPerMeter { get; }
+            public decimal MeanPointValue { get; }
+            public int FwdPoint { get; set; }
+            public int RevPoint { get; set; }
+            public int FwdAngle { get; set; }
+            public int FwdMinutes { get; set; }
+            public int FwdSeconds { get; set; }
+            public int RevAngle { get; set; }
+            public int RevMinutes { get; set; }
+            public int RevSeconds { get; set; }
+
         }
     }
 }
