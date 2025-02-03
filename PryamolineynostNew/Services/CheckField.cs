050fb10b7497c89c63dc4a9b2c7082e1cbed43db
ilexx <stealthfear@gmail.com>
2025-02-03 11:20:48 +0500

    добавлена проверка полей

---
+++ PryamolineynostNew/Services/CheckField.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace PryamolineynostNew.Services;
+
+public static class CheckField
+{
+    public static (bool, int) IsInt(TextBox textBox)
+    {
+        int result;
+        return int.TryParse(textBox.Text, out result) ? (true, result) : (false, 0);
+    }
+
+    public static (bool, decimal) IsDecimal(TextBox textBox)
+    {
+        decimal result;
+        return decimal.TryParse(textBox.Text, out result) ? (true, result) : (false, 0);
+    }
+
+    public static bool IsEmpty(TextBox textBox)
+    {
+        return string.IsNullOrEmpty(textBox.Text);
+    }
+}
