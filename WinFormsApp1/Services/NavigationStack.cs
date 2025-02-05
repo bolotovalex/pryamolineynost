5d9becbcf226163691f5daa13fc38087a2209958
ilexx <stealthfear@gmail.com>
2025-02-05 11:04:45 +0500

    Добавлеие NavigationStack для переключения между формами

---
+++ WinFormsApp1/Services/NavigationStack.cs
@@ -0,0 +1,39 @@
+﻿namespace PryamolineynostWF.Services
+{
+    public static class NavigationStack
+    {
+        private static Stack<Form> _formStack = new Stack<Form>();
+
+        public static void Navigate(Form currentForm, Form nextForm)
+        {
+            _formStack.Push(currentForm);
+            currentForm.Hide();
+            nextForm.FormClosed += (sender, events) =>
+            {
+                if (_formStack.Count > 0)
+                {
+                    var previousForm = _formStack.Pop();
+                    previousForm.Show();
+                }
+            };
+            nextForm.Show();
+        }
+
+        public static void NavigateWithData(Form currentForm, Form nextForm, Action<Form> onReturn)
+        {
+            _formStack.Push(currentForm);
+            currentForm.Hide();
+            nextForm.FormClosed += (sender, events) =>
+            {
+                if (_formStack.Count > 0)
+                {
+                    var previousForm = _formStack.Pop();
+                    onReturn(previousForm);
+                    previousForm.Show();
+                }
+            };
+            nextForm.Show();
+        }
+
+    }
+}
