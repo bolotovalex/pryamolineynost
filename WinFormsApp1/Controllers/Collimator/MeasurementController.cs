394b66c80656a4bcb0b24013e86d7b906a5cde86
ilexx <stealthfear@gmail.com>
2025-02-20 16:22:23 +0500

    Рефакторинг

---
+++ WinFormsApp1/Controllers/Collimator/MeasurementController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryamolineynostWF.Controllers.Collimator
+{
+    
+    public class MeasurementController
+    {
+        private MeasurementForm _view;
+        private DataSet _dataSet;
+        public MeasurementController(DataSet dataSet, MeasurementForm view)
+        {
+            _dataSet = dataSet;
+            _view = view;
+            //_view.dataGridView1.DataSource = _dataSet.Tables["Горизонтальная поверхность"];
+        }
+
+    }
+}
